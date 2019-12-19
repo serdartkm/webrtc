b0db0124acbdefa3f49627e4db8f71475ac06f2e
Personal Computer <personalcomputer@Personals-MacBook-Pro.local>
2019-12-19 10:58:02 +0500

    need debugging

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 /* eslint-disable react/prefer-stateless-function */
 import './style';
 import { Component } from 'preact';
-import {useRef} from 'preact/hooks';
+import { useRef, useEffect } from 'preact/hooks';
 import useLocalMediaStream from './localMedieaStream/useLocalMediaStream';
+
+const DisplayMedia = () => {
+	const { error,localMediaStream } = useLocalMediaStream({ video: true,audio: false });
+	const videoRef =useRef(null);
+
+	useEffect(() => {
+		if (localMediaStream){
+			videoRef.current.srcObject =localMediaStream;
+		}
+
+		if (error){
+			console.log('error',error);
+		}
+
+	},[localMediaStream,error]);
+
+	return (<video ref={videoRef} />);
+};
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello, World!</h1>
+				<DisplayMedia />
 			</div>
 		);
 	}
--- src/localMedieaStream/useLocalMediaStream.js
+++ src/localMedieaStream/useLocalMediaStream.js
@@ -6,7 +6,7 @@ export default async function ({ mediaStreamConstraints }){
 	const [localMediaStream, setLocalMediaStream]=useState(null);
 	const [error,setError]= useState(null);
 	useEffect(() => {
-
+			console.log("useLMS")
 		getMediaStream(mediaStreamConstraints,(error,mediaStream) => {
 			if (error){
 				setError(error);
