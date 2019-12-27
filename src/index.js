3aeb205ad08d3bcd128d57f454e6199a70c30d15
Personal Computer <personalcomputer@Personals-MacBook-Pro.local>
2019-12-27 11:48:54 +0500

    v

--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import './style';
 
 import { render } from 'preact';
 import VideoChatDemo from './video-transfer/demo-components/VideoChatDemo';
-<<<<<<< HEAD
-=======
-export default class App extends Component {
-	render() {
-		return <VideoChatDemo />;
-			
-	
-	}
-}
->>>>>>> master
+
 
 render(<VideoChatDemo />, document.getElementById('root'));
 
