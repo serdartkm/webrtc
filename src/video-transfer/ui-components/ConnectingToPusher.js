fdda8e17ef420bbf4eed3f0ea0cf3cfde07e220e
Serdar Aşırow <u13589@caliksoa.local>
2020-01-09 08:38:53 +0500

    init

--- src/video-transfer/ui-components/ConnectingToPusher.js
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact';
-import pusher from './pusher.png';
-export default function ConnectingToPusher (){
-
-	return (<div>
-		<h1>Connecting to...</h1>
-		<div> <img src={pusher} /> </div>
-	</div>);
-}
